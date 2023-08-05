d3c0ef6657e65698ec812cb267aa2453fec60059
VigneTsuki <vigne.kirara.jp@gmail.com>
2023-08-05 17:04:47 -0300

    Implementação Login

--- app/src/main/java/com/example/aplicativoteste/MainActivity.java
+++ app/src/main/java/com/example/aplicativoteste/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aplicativoteste;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    static final String Url = "18.206.155.29";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +36,81 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obter os valores de login e senha
-                String login = ((EditText) findViewById(R.id.editTextLogin)).getText().toString();
-                String senha = ((EditText) findViewById(R.id.editTextSenha)).getText().toString();
+                Login();
+            }
+        });
+    }
 
-                // Verificar o login e senha (exemplo simples, você deve implementar a lógica de autenticação)
-                if (login.equals("usuario") && senha.equals("senha")) {
-                    // Criar uma Intent para navegar para a segunda tela
-                    Intent intent = new Intent(MainActivity.this, SegundaTelaActivity.class);
-                    startActivity(intent);
+    public void Login(){
+
+        String login = ((EditText) findViewById(R.id.editTextLogin)).getText().toString();
+        String senha = ((EditText) findViewById(R.id.editTextSenha)).getText().toString();
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://" + Url + "/usuario";
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        String requestBody = "{\"login\":\"" +login+ "\", \"senha\":\""+senha+"\"}";
+        RequestBody body = RequestBody.create(mediaType, requestBody);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(responseBody.contains("true")){
+                                Intent intent = new Intent(MainActivity.this, SegundaTelaActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login ou senha incorretos.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void FuncaoTesteGet() {
+        TextView mTextViewResult = ((EditText) findViewById(R.id.editTextLogin));
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://" + Url + "/weatherforecast";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
                 }
             }
         });
